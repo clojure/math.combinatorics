67b2e2a38be69fa25e713594c6da74303d52042c
Mark Engelberg <mark.engelberg@gmail.com>
2014-07-19 15:00:10 -0700

    perf improvement in sorted-numbers?
    sorted-numbers? is called once at the beginning of the permutations
    function in order to determine which version of the permutations
    algorithm to use -- a more efficient algorithm can be chosen if the
    numbers are already sorted.  Since this function is only called once,
    improving its performance will have a negligible effect on the overall
    performance of permutations.  But this is an improvement, so why not?

--- src/main/clojure/clojure/math/combinatorics.clj
+++ src/main/clojure/clojure/math/combinatorics.clj
@@ -174,7 +174,7 @@ In prior versions of the combinatorics library, there were two similar functions
   "Returns true iff s is a sequence of numbers in non-decreasing order"
   [s]
   (and (every? number? s)
-       (every? (partial apply <=) (partition 2 1 s))))
+       (or (empty? s) (apply <= s))))
 
 (defn- multi-perm
   "Handles the case when you want the permutations of a list with duplicate items."
