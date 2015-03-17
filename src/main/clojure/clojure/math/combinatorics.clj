51b11a6b70e4b824120f9553316c9a61474c36f8
Mark Engelberg <mark.engelberg@gmail.com>
2015-03-16 22:03:02 -0700

    index-combinations now based on Alg T

--- src/main/clojure/clojure/math/combinatorics.clj
+++ src/main/clojure/clojure/math/combinatorics.clj
@@ -71,26 +71,7 @@ Most of these algorithms are derived from algorithms found in Knuth's wonderful 
 "
 )
 
-(defn- index-combinations
-  [n cnt]
-  (lazy-seq
-   (let [c (vec (cons nil (for [j (range 1 (inc n))] (+ j cnt (- (inc n)))))),
-         iter-comb
-         (fn iter-comb [c j]
-           (if (> j n) nil
-             (let [c (assoc c j (dec (c j)))]
-               (if (< (c j) j) [c (inc j)]
-                 (loop [c c, j j]
-                   (if (= j 1) [c j]
-                     (recur (assoc c (dec j) (dec (c j))) (dec j)))))))),
-         step
-         (fn step [c j]
-           (cons (rseq (subvec c 1 (inc n)))
-                 (lazy-seq (let [next-step (iter-comb c j)]
-                             (when next-step (step (next-step 0) (next-step 1)))))))]
-     (step c 1))))
-
-(defn- index-combinations2 ;Algorithm T
+(defn- index-combinations ;Algorithm T
   [n cnt]
   (lazy-seq
    (let [c (vec (concat [nil]
@@ -125,7 +106,7 @@ Most of these algorithms are derived from algorithms found in Knuth's wonderful 
               (= n 1) (for [item items] (list item))
               (= n cnt) (list (seq items))
               :else
-              (map #(map v-items %) (index-combinations2 n cnt)))))))
+              (map #(map v-items %) (index-combinations n cnt)))))))
 
 (defn- unchunk
   "Given a sequence that may have chunks, return a sequence that is 1-at-a-time
