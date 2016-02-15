055853e6e83d4a082973fa5d0747d18042541ce0
jurgenphotek <jurgen.photek@gmail.com>
2016-02-15 22:59:02 +0000

    Create project.clj

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject clojure.math.combinatorics "0.0.5-SNAPSHOT"
+  :url "https://github.com/clojure/math.combinatorics/"
+  :source-paths ["src/main/clojure"]
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :pom-addition [:developers [:developer [:name "Mark Engelberg"]]])
