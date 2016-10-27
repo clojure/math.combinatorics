34d4840d503ab4e54221c7f759b37cb9f4db148b
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2016-10-27 19:22:26 -0400

    infer spec

---
+++ src/test/clojure/clojure/math/infer_specs_combinatorics.clj
@@ -0,0 +1,4 @@
+(ns clojure.math.infer-specs-combinatorics
+  (:require [clojure.math.infer-combinatorics :as i]))
+
+(i/infer :spec)
--- src/test/clojure/clojure/math/infer_types_combinatorics.clj
+++ src/test/clojure/clojure/math/infer_types_combinatorics.clj
@@ -1,34 +1,4 @@
 (ns clojure.math.infer-types-combinatorics
-  (:use clojure.test)
-  (:require [clojure.core.typed :as t]
-            [clojure.core.typed.runtime-infer :as infer]))
+  (:require [clojure.math.infer-combinatorics :as i]))
 
-(t/install #{:load})
-
-(defn delete-anns [nss]
-  (doseq [ns nss]
-    (infer/delete-generated-annotations
-      ns
-      {:ns ns})))
-
-(defn infer-anns [nss]
-  (doseq [ns nss]
-    (t/runtime-infer :ns ns)))
-
-(def infer-files
-  '[clojure.math.combinatorics
-    ])
-
-;; FIXME shouldn't need this, but some types
-;; don't compile
-(delete-anns infer-files)
-
-(def tests 
-  '[clojure.math.test-combinatorics
-    ])
-
-;; FIXME need to forcibly reload the :lang'd file. Why?
-(apply require (conj tests :reload-all))
-(apply run-tests tests)
-
-(infer-anns infer-files)
+(i/infer :type)
